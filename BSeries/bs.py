ea08a4a1298a2e8bc6fc96fc2ee7eb7e1feffad8
David Ketcheson <dketch@gmail.com>
2021-03-31 10:21:13 +0300

    Everything functioning for modified equation analysis.

--- BSeries/bs.py
+++ BSeries/bs.py
@@ -1,6 +1,193 @@
 """
 A library for working with B-series.
 """
+
+indices = 'ijklmnpqrstuvwxyz'
+import sympy
+h = sympy.Symbol('h')
+y = sympy.Symbol('y')
+
+import numpy as np
+
+def object_einsum(string, *arrays):
+    """
+    Simplified object einsum, without much error checking.
+    Stolen from https://gist.github.com/seberg/5236560, with some modifications.
+    We can remove this and rely directly on np.einsum after the related
+    PR is merged to numpy.
+    """
+    import copy
+    try:
+        return np.einsum(string, *arrays)
+    except TypeError:
+        pass
+    
+    s = string.split('->')
+    in_op = s[0].split(',')
+    out_op = None if len(s) == 1 else s[1].replace(' ', '')
+
+    in_op = [axes.replace(' ', '') for axes in in_op]
+    all_axes = set()
+    repeated_axes = set()
+    
+    for axes in in_op:
+        list(repeated_axes.update(ax) for ax in axes if ax in all_axes)
+        all_axes.update(axes)
+
+    if out_op is None:
+        out_op = set(sorted(all_axes))
+        list(out_op.discard(rep_ax) for rep_ax in repeated_axes)
+    else:
+        all_axes.update(out_op)
+    
+    perm_dict = {_[1]: _[0] for _ in enumerate(all_axes)}
+    
+    dims = len(perm_dict)
+    op_axes = []
+    for axes in (in_op + list((out_op,))):
+        op = [-1] * dims
+        for i, ax in enumerate(axes):
+           op[perm_dict[ax]] = i
+        op_axes.append(op)
+    
+    op_flags = [('readonly',)] * len(in_op) + [('readwrite', 'allocate')]
+    dtypes = [np.object_] * (len(in_op) + 1) # cast all to object
+
+    nditer = np.nditer(arrays + (None,), op_axes=op_axes, flags=['buffered', 'delay_bufalloc', 'reduce_ok', 'grow_inner', 'refs_ok'], op_dtypes=dtypes, op_flags=op_flags)
+
+    nditer.operands[-1][...] = 0
+    nditer.reset()
+    
+    for vals in nditer:
+        out = vals[-1]
+        prod = copy.deepcopy(vals[0])
+        #prod = vals[0]
+        for value in vals[1:-1]:
+            prod *= value
+        out += prod
+    
+    return nditer.operands[-1]
+
+class BSeries(object):
+
+    def __init__(self, coeffs=None, F=None):
+        import sympy
+        self.coeffs = coeffs
+        if F:
+            self.F = F
+        else:
+            self.F = sympy.Function('F')
+
+    def eval(self, order=2):
+        import sympy
+        from BSeries import trees
+        if self.coeffs:
+            value = y
+            for i in range(1,order+1):
+                value += h**i/sympy.factorial(i)*sum([t.alpha()*self.coeffs(t)*self.F(t) for t in trees.all_trees(i)])
+            return value
+
+    def print(self, order=2):
+        from BSeries import trees
+        if self.F and (self.coeffs is None):
+            # print elementary differentials with unknown coeffs
+            pass
+        elif self.coeffs and self.F is None:
+            # print elementary weights and unknown differentials
+            outstr = r'y\\'
+            for p in range(1,order+1):
+                outstr += r' + h^{}/{}! ('.format(p,p)
+                for i, tree in enumerate(trees.all_trees(p)):
+                    outstr += str(tree.alpha()*self.coeffs(tree))
+                    outstr += ' F(t_{{ {}{} }})(y)'.format(p,i+1)
+                    outstr += r'+'
+                outstr = outstr[:-1]
+                outstr += r')\\'
+            return outstr
+
+class TreeMap(dict):
+
+    def __init__(self, symbol='a'):
+        self.symbol = symbol
+
+    def __missing__(self, key):
+        from BSeries import trees
+        from sympy import Symbol
+        tree_name = next(name for name, tree in trees.canonical_forest.items() if tree == key)
+        tree_index = tree_name[1:]
+        self[key]=sympy.Symbol(self.symbol+tree_index)
+        return self[key]
+
+    def __call__(self, key):
+        return self[key]
+
+
+def RK_BSeries(method):
+    cfun = lambda tree: tree.density() * elementary_weight(tree, method.A, method.b)
+    return BSeries(coeffs=cfun)
+
+def generic_elementary_differential_string(tree):
+    for i, node in enumerate(tree.nodes):
+        node.label = indices[i]
+    factors = []
+    for node in tree.nodes:
+        factor = 'f^{}'.format(node.label)
+        if node.children:
+            factor += '_{'
+            for child in node.children:
+                factor += child.label
+            factor += '}'
+        factors.append(factor)
+
+    return ' '.join(factors)
+
+def elementary_differential(tree, f, u):
+    from sympy import Derivative as D
+    N = len(f); M = len(u);
+    F1 = np.empty((N,M), dtype=object)
+    for i in range(N):
+        for j in range(M):
+            F1[i,j] = D(f[i],u[j])
+
+    root = tree.nodes[0]
+    if root.children is None:
+        return f
+    elif len(root.children) == 1:
+        return sum([F1[:,j]*sub_differential(node.children[0], tree.nodes, f)[j] for j in range(N)])
+    else:
+        return
+    pass
+
+
+def elementary_weight_einsum(tree, A, b):
+    # This code will work with plain np.einsum() after
+    # https://github.com/numpy/numpy/pull/18053 gets merged
+    import numpy as np
+    c = np.sum(A,1)
+    inds = []
+    tensors = []
+    counter = 0
+    if tree.nodes[0].children is None:
+        return sum(b)
+    for i, node in enumerate(tree.nodes):
+        if i==0: 
+            tensors.append(b)
+            inds.append(indices[0])
+            node.label = indices[0]
+            counter += 1
+        else:
+            if node.children:
+                tensors.append(A)
+                node.label = indices[counter]
+                inds.append(node.parent.label + node.label)
+                counter += 1
+            else:
+                tensors.append(c)
+                inds.append(node.parent.label)
+    return object_einsum(','.join(inds), *tensors)
+    #return np.einsum(','.join(inds), *tensors, optimize='optimal')
+
+
 def elementary_weight(tree, A, b):
     """
     Compute the elementary weight corresponding to a given tree and set of
@@ -29,3 +216,20 @@ def subweight_vector(node, nodelist, A):
         return sum([A[:,j]*np.prod([subweight_vector(child, nodelist, A) for child in node.children],0)[j] for j in range(A.shape[0])])
 
 
+def subs(b, a, t, args='trees'):
+    """
+    Returns the coefficient of t in the B-series that is formed
+    by substituting the B-series b into the B-series a.
+    """
+    from BSeries import trees
+    from functools import reduce
+    from operator import mul
+
+    forests, skeletons = t.all_partitions()
+    expr = 0
+    for forest, skeleton in zip(forests, skeletons):
+        if args == 'trees':
+            expr += reduce(mul,[b(tree) for tree in forest])*a(skeleton)        
+        elif args == 'integers':
+            expr += reduce(mul,[b(trees.intmap(tree)) for tree in forest])*a(skeleton)        
+    return expr
--- BSeries/trees.py
+++ BSeries/trees.py
@@ -1,3 +1,4 @@
+
 class Node(object):
     """
     A single node of a tree.
@@ -48,29 +49,33 @@ def make_nodelist(root):
     Recursively finds all the nodes that are descendants of root and returns
     them as a list.
     """
-    nodelist = [root]
+    node_list = [root]
     if root.children is not None:
         for child in root.children:
-            nodelist += make_nodelist(child)
-    return nodelist
+            node_list += make_nodelist(child)
+    return node_list
 
 class RootedTree(object):
     r"""
     Represents a rooted tree.  Maintains two equivalent representations,
     one as a nested list and the other as a set of Nodes each with a
-    parent and zero or more children.
+    parent and zero or more children.  The tree is essentially viewed
+    as unlabeled/unordered; equality is defined in this sense.
+    But the order of the list of nodes can be used as a labeling.
 
     Can be initialized in three ways:
 
-        1. From a nest list.
+        1. From a nested list.
         2. From a set of Nodes.
         3. From a level sequence.
 
     To do: add examples.
     """
 
-    def __init__(self,initializer):
+    def __init__(self, initializer, name=None):
         import numpy as np
+
+        self.name = name
 
         if initializer == []:
             self._nl = []
@@ -95,9 +100,10 @@ class RootedTree(object):
             if self.root.children:
                 for child in self.root.children:
                     self._nl.append(generate_nested_list(child,self.nodes))
+            self._nl = sorted_tree(self._nl)
 
         elif type(initializer[0]) is Node:
-            # Initialize from nodelist
+            # Initialize from node_list
             self.nodes = initializer
             self.root = self.nodes[0]
             # Now form nested list
@@ -105,6 +111,7 @@ class RootedTree(object):
             if self.root.children:
                 for child in self.root.children:
                     self._nl.append(generate_nested_list(child,self.nodes))
+            self._nl = sorted_tree(self._nl)
 
         else:   # Initialize from nested list
             self._nl = initializer  # Nested list form
@@ -119,12 +126,29 @@ class RootedTree(object):
                 counter = generate_child_nodes(child, child_node, counter)
 
             self.nodes = make_nodelist(self.root)
+            self._nl = sorted_tree(self._nl)
+
+    def __hash__(self):
+        # Required so that Trees can be dict keys
+        return hash(to_tuple(sorted_tree(self._nl)))
+
+    def __repr__(self):
+        if self.name: return self.name
+        else:
+            nl = sorted_tree(self._nl)
+            try:
+                return next(name for name, tree in canonical_forest.items() if tree._nl == nl)
+            except:
+                return str(self._nl)
 
     def subtrees(self):
         return [RootedTree(nl) for nl in self._nl]
 
     def __len__(self):
         return len(self.nodes)
+
+    def copy(self):
+        return RootedTree(self._nl)
 
     def order(self):
         return len(self.nodes)
@@ -172,8 +196,16 @@ class RootedTree(object):
             sigma *= factorial(m)*RootedTree(tree).symmetry()**m
         return sigma
 
+    def alpha(self):
+        r"""
+        The number of elements of the equivalence class of the tree;
+        i.e., the number of possible different monotonic labelings.
+        """
+        return len(self.distinct_labelings())
+
     def __eq__(self, tree2):
-        return self._nl == tree2._nl
+        # This is not correct!
+        return sorted_tree(self._nl) == sorted_tree(tree2._nl)
 
     def plot(self):
         import matplotlib.pyplot as plt
@@ -217,12 +249,84 @@ class RootedTree(object):
         return forest
 
 
-def plot_labeled_tree(nodelist):
+    def all_partitions(self):
+        """
+        Compute all partitions of a tree, as defined in Section 2.3
+        of Chartier, Hairer, & Vilmart (2010).
+        """
+        forests = []
+        skeletons = []
+        num_edges = len(self)-1
+        for i in range(2**num_edges):  # Loop over all partitions
+            edge_set = bin(i)[2:].zfill(num_edges)
+            # edge_set is a string where '0' indicates that the edge leading to
+            # the node in that place is removed.
+            forest = partition_forest(self, edge_set)
+            skeleton = partition_skeleton(self, edge_set)
+            forests.append(forest)
+            skeletons.append(skeleton)
+
+        return forests, skeletons
+
+
+def partition_forest(tree, edge_set):
+    """
+    The partition forest of the tree represented by node_list
+    is the set of trees that result when the edges marked by
+    zero in edge_set are removed.
+    """
+    import numpy as np
+    # first make a copy
+    node_list = tree.copy().nodes
+    # Now sever the indicated edges
+    for i, node in enumerate(node_list[1:]):
+        if edge_set[i] == '0':
+            node.parent.children.remove(node)
+    # Now assemble the forest
+    forest = []
+    included = np.zeros(len(tree))
+    for i, node in enumerate(node_list):
+        if included[i] == 0:
+            tree_nodes = make_nodelist(node)
+            forest.append(RootedTree(tree_nodes))
+            for tree_node in tree_nodes:
+                included[node_list.index(tree_node)] = 1
+    return forest
+
+def partition_skeleton(tree, edge_set):
+    """
+    The partition skeleton is the tree obtained by contracting each
+    tree of the partition forest to a single vertex and then replacing
+    the edges of the tree.
+
+    The way this is performed here is: for each edge that is "kept"
+    in edge_set, the child node is removed and its children are
+    given to the parent (i.e. to their grandparent), recursively.
+    """
+    from copy import copy
+    # first make a copy
+    nodes = tree.copy().nodes
+    nodes = nodes[::-1]  # We want to loop through children before parents
+    edge_set = edge_set[::-1]
+    for i, node in enumerate(nodes[:-1]):
+        if edge_set[i] == '1':
+            if node.children:
+                node.parent.children += node.children
+            node.parent.children.remove(node)
+
+    nodes = nodes[::-1]
+    edge_set = edge_set[::-1]
+    skeleton_node_indices = [0] + [i+1 for i in range(len(edge_set)) if edge_set[i]=='0']
+    skeleton_nodes = [nodes[i] for i in skeleton_node_indices]
+    return RootedTree(skeleton_nodes)
+
+
+def plot_labeled_tree(node_list):
     # Plot based on node structure
     import matplotlib.pyplot as plt
     plt.scatter([0],[0],c='k',marker='o')
-    root = nodelist[0]
-    if root.children is not None: _plot_labeled_subtree(nodelist,0,0,0,1.)
+    root = node_list[0]
+    if root.children is not None: _plot_labeled_subtree(node_list,0,0,0,1.)
     xlim = plt.xlim()
     if xlim[1]-xlim[0]<1:
         plt.xlim(-0.5,0.5)
@@ -231,12 +335,12 @@ def plot_labeled_tree(nodelist):
     plt.axis('off')
 
 
-def _plot_labeled_subtree(nodelist, root_index, xroot, yroot, xwidth):
+def _plot_labeled_subtree(node_list, root_index, xroot, yroot, xwidth):
     import matplotlib.pyplot as plt
     import numpy as np
     plt.scatter(xroot,yroot,c='k')
     plt.text(xroot+0.05,yroot,str(root_index),ha='left')
-    root = nodelist[root_index]
+    root = node_list[root_index]
     if root.children is None: return
     ychild = yroot + 1
     nchildren = len(root.children)
@@ -244,19 +348,19 @@ def _plot_labeled_subtree(nodelist, root_index, xroot, yroot, xwidth):
     xchild = np.linspace(xroot-dist, xroot+dist, nchildren)
     for i, child in enumerate(root.children):
         plt.plot([xroot,xchild[i]], [yroot,ychild], '-k')
-        ichild = nodelist.index(child)
-        _plot_labeled_subtree(nodelist,ichild,xchild[i],ychild,xwidth/3.)
+        ichild = node_list.index(child)
+        _plot_labeled_subtree(node_list,ichild,xchild[i],ychild,xwidth/3.)
 
-def generate_all_labelings(nodelist):
+def generate_all_labelings(node_list):
     """
     Returns a list of all permissible orderings of the nodes.  A permissible
     ordering is one in which each child comes after its parent.
     """
-    labeled = [[nodelist[0]]]
-    for i in range(1,len(nodelist)):
+    labeled = [[node_list[0]]]
+    for i in range(1,len(node_list)):
         new_labeled = []
         for base in labeled:
-            for node in nodelist[1:]:
+            for node in node_list[1:]:
                 if (node.parent in base) and (node not in base):
                     basecopy = base.copy()
                     basecopy.append(node)
@@ -264,7 +368,7 @@ def generate_all_labelings(nodelist):
         labeled = new_labeled.copy()
     return labeled
 
-def generate_distinct_labelings(nodelist):
+def generate_distinct_labelings(node_list):
     """
     Returns a list of all non-equivalent permissible orderings of the nodes.  A
     permissible ordering is one in which each child comes after its parent.
@@ -272,24 +376,24 @@ def generate_distinct_labelings(nodelist):
     equivalent nodes.  Two nodes are equivalent if they have the same parent
     and identical subtrees.
     """
-    labeled = [[nodelist[0]]]
+    labeled = [[node_list[0]]]
     equivalence_set = []
-    for i, node in enumerate(nodelist):
+    for i, node in enumerate(node_list):
         es = []
         if i == 0: equivalence_set.append(es)
         else:
-            nlform = generate_nested_list(node,nodelist)
+            nlform = generate_nested_list(node,node_list)
             for child in node.parent.children:
-                if generate_nested_list(child,nodelist)==nlform and nodelist.index(child) < i:
+                if generate_nested_list(child,node_list)==nlform and node_list.index(child) < i:
                     es.append(child)
             equivalence_set.append(es)
         
-    for i in range(1,len(nodelist)):
+    for i in range(1,len(node_list)):
         new_labeled = []
         for base in labeled:
-            for node in nodelist[1:]:
+            for node in node_list[1:]:
                 if (node.parent in base) and (node not in base):
-                    if all([n in base for n in equivalence_set[nodelist.index(node)]]):
+                    if all([n in base for n in equivalence_set[node_list.index(node)]]):
                         basecopy = base.copy()
                         basecopy.append(node)
                         new_labeled += [basecopy]
@@ -339,11 +443,17 @@ def all_trees(order):
       Beyer, Terry, and Sandra Mitchell Hedetniemi.
       "Constant time generation of rooted trees."
       SIAM Journal on Computing 9.4 (1980): 706-712.
+
+    Note that the ordering of the trees matches the tables from
+    Hairer's books up to order 4, but differs at order 5.
     """
     forest = []
+    if order == 1: return forest
     s = list(range(order))
+    i = 0
     while s is not None:
-        forest.append(RootedTree(s))
+        i += 1
+        forest.append(RootedTree(s,name='t{}{}'.format(order,i)))
         s = get_successor(s)
 
     return forest[::-1]
@@ -366,3 +476,60 @@ def get_successor(s):
             successor += Tq
         successor += Tq[:(len(s)-len(successor))]
         return successor
+
+def sorted_tree(ls):
+    """
+    Recursively sort a nested list to get a canonical version.
+    """
+    for i in range(len(ls)):
+        if type(ls[i]) is list:
+            ls[i] = sorted_tree(ls[i])
+    ls.sort()
+    return ls
+
+def to_tuple(lst):
+    return tuple(to_tuple(i) if isinstance(i, list) else i for i in lst)
+
+def gamma(t):
+    return t.density()
+
+def intmap(t):
+    """
+    Canonical map from trees to integers.
+    """
+    if t == RootedTree([]): return 1
+    if t == RootedTree([[]]): return 2
+    if t == RootedTree([[],[]]): return 3
+    if t == RootedTree([[[]]]): return 4
+    if t == RootedTree([[],[],[]]): return 5
+    if t == RootedTree([[],[[]]]): return 6
+    if t == RootedTree([[[],[]]]): return 7
+    if t == RootedTree([[[[]]]]): return 8
+    if t == RootedTree([[],[],[],[]]): return 9
+    if t == RootedTree([[],[],[[]]]): return 10
+    if t == RootedTree([[],[[],[]]]): return 11
+    if t == RootedTree([[],[[[]]]]): return 12
+    if t == RootedTree([[[]],[[]]]): return 13
+    if t == RootedTree([[[],[],[]]]): return 14
+    if t == RootedTree([[[],[[]]]]): return 15
+    if t == RootedTree([[[[],[]]]]): return 16
+    if t == RootedTree([[[[[]]]]]): return 17
+
+canonical_forest = {}
+canonical_forest['t1'] = RootedTree([])
+canonical_forest['t2'] = RootedTree([[]])
+canonical_forest['t31'] = RootedTree([[],[]])
+canonical_forest['t32'] = RootedTree([[[]]])
+canonical_forest['t41'] = RootedTree([[],[],[]])
+canonical_forest['t42'] = RootedTree([[],[[]]])
+canonical_forest['t43'] = RootedTree([[[],[]]])
+canonical_forest['t44'] = RootedTree([[[[]]]])
+canonical_forest['t51'] = RootedTree([[],[],[],[]])
+canonical_forest['t52'] = RootedTree([[],[],[[]]])
+canonical_forest['t53'] = RootedTree([[],[[],[]]])
+canonical_forest['t54'] = RootedTree([[],[[[]]]])
+canonical_forest['t55'] = RootedTree([[[]],[[]]])
+canonical_forest['t56'] = RootedTree([[[],[],[]]])
+canonical_forest['t57'] = RootedTree([[[],[[]]]])
+canonical_forest['t58'] = RootedTree([[[[],[]]]])
+canonical_forest['t59'] = RootedTree([[[[[]]]]])
