c4d25ff86b4e52cc366adc8413f6c4e9d01c722f
David Ketcheson <dketch@gmail.com>
2021-09-08 14:33:00 +0300

    Use Readme as description on Pypi.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import setuptools
 from distutils.core import setup
 
+# Use README as description on Pypi
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='BSeries',
       version='0.1',
       packages=['BSeries'],
@@ -10,4 +15,6 @@ setup(name='BSeries',
       description='A Package for Maniuplating Butcher Series',
       license='modified BSD',
       install_requires=['numpy','sympy','matplotlib'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       )
